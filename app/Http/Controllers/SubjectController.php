718e1eaed6cd60c50de5deae73e35fff3e142fc2
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2024-10-18 10:41:58 +0200

    orakozben

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Subject;
 
 class SubjectController extends Controller
 {
-    //
+    public function index(){
+        return view('subjects.create');
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'subject_name' => 'required|string|max:255',
+        ]);
+        Subject::create($request->all());
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubjectController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/newsubject', [SubjectController::class, 'index'])->name('subject.index');
+Route::post('newsubject', [SubjectController::class, 'store'])->name('subject.store');
+
