394d3d30420e0810ea781f6677a37765da02e211
Császár Martin Krisztián <145013640+Csasziuser@users.noreply.github.com>
2024-11-08 09:10:17 +0100

    orakozben1108

--- resources/views/test/show.blade.php
+++ resources/views/test/show.blade.php
@@ -6,7 +6,44 @@
     <title>Teszt</title>
 </head>
 <body>
+    @if ($errors->any())
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+            @endforeach
+        </ul>
+    @endif
+    @if (@session('success'))
+        <p>{{session('success')}}</p>
+    @endif
+
     <h1>{{$subject->subject_name}} teszt</h1>
+
+    <form action="" method="POST">
+        @csrf
+        <label for="email">E-mail: </label>
+        <input type="email" name="email" id="email"><br><br>
+
+        <ul>
+            @foreach ($questions as $question)
+
+                <li>
+                    <p>{{$question->question_text}}</p>
+                    <input type="hidden" name="question_id" value="{{$question->id}}">
+                    <ul>
+                        @foreach ($question->answers as $answer)
+                            <li>
+                                <input type="checkbox" name="answers[{{$question->id}}][]" value="{{$answer->id}}" id="answer{{$answer->id}}">
+                                <label for="answer{{$answer->id}}">{{$answer->answer_text}}</label>
+                            </li>
+                        @endforeach
+                    </ul>
+                </li>
+            
+            @endforeach
+        </ul>
+        <button type="submit">Beadás</button>
+    </form>
 
     
 </body>
